0e4264aff2d590149d65c49cb4e8277f9e576581
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-07-21 18:27:37 +0100

    weather api data

---
+++ weather/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from .models import WeatherUser
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['location'] = user.location
+        token['days'] = user.days
+
+        # token['password'] = user.password
+        # token['password_confirmation'] = user.password_confirmation
+        # ...
+
+        return token
+
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
+
+
+class WeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeatherUser
+        # fields = ['id', 'location']
---
+++ weather/views.py
@@ -0,0 +1,85 @@
+import re
+from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer
+import requests
+import json
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(('GET',))
+@permission_classes([IsAuthenticated])
+def authenticatedwetheruser(request):
+    key = '9489e1da28ff4283927154211221907'
+    data = request.user
+    location = data.location
+    weather_request = requests.get("https://api.weatherapi.com/v1/current.json?key="+key+"&q="+location)
+    data = json.loads(weather_request.content)
+    return Response(data)
+
+
+@api_view(('POST',))
+@csrf_exempt
+def unauthenticatedwetheruser(request):
+    key = '9489e1da28ff4283927154211221907'
+    location = request.data['location']
+    if location is not None:
+        URL = 'https://api.weatherapi.com/v1/current.json'
+        PARAMS = {'key': key ,'q' : location}
+        weather_request = requests.get(url = URL,params=PARAMS)
+        # weather_request = requests.get("https://api.weatherapi.com/v1/current.json?key="+key+"&q="+location)
+        data = json.loads(weather_request.content)
+        return Response(data, status = status.HTTP_200_OK)    
+
+    else: 
+        notfound = 'Kindly enter a valid location'
+        return Response(notfound, status = status.HTTP_400_BAD_REQUEST) 
+
+
+@api_view(('GET','POST'))
+@permission_classes([IsAuthenticated])
+def authenticatedwetheruser_with_days(request):
+    key = '9489e1da28ff4283927154211221907'
+    data = request.user
+    location = data.location
+    days = request.data['days']
+    number_of_days_allowed = 10
+    if days <= number_of_days_allowed:
+        converted_days = str(days)
+        weather_request = requests.get("https://api.weatherapi.com/v1/forecast.json?key="+key+"&q="+location+"&days="+converted_days)
+        data = json.loads(weather_request.content)
+        return Response(data)
+    else:
+        notfound = 'Kindly Enter a value less than or equal to 10'
+        return Response(notfound, status = status.HTTP_404_NOT_FOUND) 
+    
+
+@api_view(('POST',))
+@csrf_exempt
+def unauthenticatedwetheruser_with_days(request):
+    key = '9489e1da28ff4283927154211221907'
+    location = request.data['location']
+    days = request.data['days']
+    number_of_days_allowed = 10
+    if days <= number_of_days_allowed:
+        URL = 'https://api.weatherapi.com/v1/forecast.json'
+        PARAMS = {'key': key ,'q' : location, 'days': days}
+        weather_request = requests.get(url = URL,params=PARAMS)
+        data = json.loads(weather_request.content)
+        return Response(data, status = status.HTTP_200_OK)
+    else:
+        notfound = 'Kindly Enter a value less than or equal to 10'
+        return Response(notfound, status = status.HTTP_404_NOT_FOUND) 
+        
+
+
+     
