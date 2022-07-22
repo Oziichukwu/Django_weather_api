5cdff9025b08d0da6f009d1a38de42c570351433
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-07-22 10:38:07 +0100

    added heroku app to allowed_host

--- weatherApi/settings.py
+++ weatherApi/settings.py
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wh=m*u6-nq_cb=47hydk*xa+15+9zabza40j__exs1-dmk9d)g'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoweatherapi.herokuapp.com']
 
 
 # Application definition
